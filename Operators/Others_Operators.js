d8111bbf61873691fce6779c74c4bc36ef701492
Linkcar13 <carlos.estrada01@epn.edu.ec>
2021-10-29 18:48:51 -0500

    CursoJs

--- Operators/Others_Operators.js
+++ Operators/Others_Operators.js
@@ -7,7 +7,7 @@ console.log(NameC);
 // Coditional Operator (condition ? value1:value2)
 console.log(2==3 ? 'son iguales':'son diferentes');
 
-//Operador de Destructuración
+//Operador de Desestructuración
  var Dog={
 age:'3 months',
 state:'in adoption'
@@ -15,3 +15,24 @@ state:'in adoption'
   var {age}=Dog;
   console.log(Dog);
   console.log(age);
+//Desestructurar Arrays
+var arreglo=[1,2,3,4]; 
+var [first,,third]=arreglo;
+console.log(first);
+console.log(third);
+
+
+//poperty access
+
+//Point operartor 
+var persona= {
+nombre:'Pato',
+apellido:'Ganso'
+}
+console.log(persona.nombre);
+
+//[] Notation 
+console.log(persona['nombre']);
+
+// Data Type operator
+console.log(typeof'Dog');
