5d029283ea7248588a876f4828f18309752bb507
Linkcar13 <carlos.estrada01@epn.edu.ec>
2021-10-29 17:32:18 -0500

    CursoJs

---
+++ Operators/Logical_Operators.js
@@ -0,0 +1,13 @@
+/**
+ * LOGICAL OPERATORS
+ */
+
+//AND &&
+console.log(true && true);
+console.log(3==3&&1==2);
+//OR || 
+console.log(true || true);
+console.log(3==3||1==2);
+
+//NOT !
+console.log(!(true || true));
---
+++ Operators/Others_Operators.js
@@ -0,0 +1,17 @@
+// String Operator (Concatenar) (+)
+var name1= 'Carlos';
+var lastname='Estrada';
+
+var NameC= name1 + ' ' + lastname;
+console.log(NameC);
+// Coditional Operator (condition ? value1:value2)
+console.log(2==3 ? 'son iguales':'son diferentes');
+
+//Operador de Destructuración
+ var Dog={
+age:'3 months',
+state:'in adoption'
+ };
+  var {age}=Dog;
+  console.log(Dog);
+  console.log(age);
