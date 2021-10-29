5fa3fc726d2c4a39f9c8624c9aad9cfb53bc7876
Linkcar13 <carlos.estrada01@epn.edu.ec>
2021-10-29 18:59:59 -0500

    CursoJs

---
+++ Test1/test1.js
@@ -0,0 +1,23 @@
+/**
+ * Escribir un programa que calcule el número de segundos que existen en un día. */
+// dia tiene 24 horas, cada hora tiene 60 min y cada min tiene 60 seg.
+console.log('un dia tiene '+(24*(60**2))+' segundos');
+/**
+ * Realiza un programa que a partir de los valores ancho=2 y alto=5,
+
+calcule el área de un rectángulo.
+ */
+
+var ancho=2;
+var alto=5;
+var areaR;
+areaR=ancho*alto;
+console.log('El área del rectángulo es: '+areaR);
+
+/**
+ * Hacer un conversor de grados centígrados a grados Fahrenheit.
+ */
+//Factor de conversion: 9/5 +32
+var grados =29; // modificar esta línea de se rnecesario 
+var GradosF=(grados*9/5)+32;
+console.log('en F°: '+GradosF);
