ee451ee91ea571a771875c13d29f28e6ede310d5
Linkcar13 <carlos.estrada01@epn.edu.ec>
2021-10-29 16:56:43 -0500

    CursoJs

---
+++ Operators/Arithmethic_Operators.js
@@ -0,0 +1,16 @@
+/**
+ * ARITHMETHIC OPERATORS
+ */
+
+// +,-,*,/,%,**
+console.log(3+3); 
+console.log(3-3);
+console.log(3*3);
+console.log(7/3);
+console.log(56%3);
+console.log(4**3);
+ // ++,-- increment discrement operators
+ var x=0;
+ console.log(++x);
+ var y=4;
+ console.log(--y);
